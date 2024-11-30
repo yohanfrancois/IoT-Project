acb8346c33ccded840cb305b8966f7c4338d88b7
ValT78 <118465560+ValT78@users.noreply.github.com>
2024-11-30 14:45:23 +0100

    Add Bug collectible + bouton reset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public bool unlockedJump;
-    public bool unlockedWallJump;
-    public bool unlockedPlatform;
-    public bool unlockedInventory;
+    public bool unlockedPlatform { get; private set; }
+    public bool unlockedJump { get; private set; }
+    public bool unlockedInventory { get; private set; }
+    public bool unlockedWallJump { get; private set; }
+    public bool canResetToMenu { get; private set; }
+
 
 
     private void Awake()
@@ -22,7 +24,35 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-        }
+        } 
+    }
+
+    public void SetUnlockedPlatform(bool value)
+    {
+        unlockedPlatform = value;
+    }   
+
+    public void SetUnlockedJump(bool value)
+    {
+        unlockedJump = value;
+    }
+
+    public void SetUnlockedInventory(bool value)
+    {
+        unlockedInventory = value;
+    }
+    public void SetUnlockedWallJump(bool value)
+    {
+        unlockedWallJump = value;
+    }
+    public void SetCanResetToMenu(bool value)
+    {
+        canResetToMenu = value;
+    }
+
+    public void ResetGame()
+    {
+        canResetToMenu = false;
     }
 
 }

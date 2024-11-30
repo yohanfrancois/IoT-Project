749b3005b4bd24c99affb24a4af1957bc6c12e7c
greenreeper1 <cedricba.gautier@gmail.com>
2024-11-30 22:05:24 +0100

    merge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -123,6 +123,17 @@ public class GameManager : MonoBehaviour
                 renderer.color = new Color(1f,1f,1f,1f);
             }
         }
+        else
+        {
+            GameObject[] unityObjects = GameObject.FindGameObjectsWithTag("Plateforme");
+            // Loop through each object and perform an action
+            foreach (GameObject obj in unityObjects)
+            {
+                // Perform an action on each object, for example:
+                SpriteRenderer renderer = obj.GetComponent<SpriteRenderer>();
+                renderer.color = new Color(1f, 1f, 1f, 0f);
+            }
+        }
     }
 
 }

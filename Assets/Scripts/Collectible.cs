f5b6983e6fc9fa48916b1669e456fb2233b1c388
Wangha <132218157+Arwangha@users.noreply.github.com>
2024-11-30 13:04:25 +0100

    Half-inventory

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,9 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer highlighterSpriteRenderer;
-    [SerializeField] private GameObject inventorySprite;
+    //[SerializeField] private GameObject inventorySprite;
+    private static List<Collectible> _collectibles;
+    private bool _mouseHovering;
+
+    private void Awake()
+    {
+        _collectibles ??= new List<Collectible>();
+    }
+
+    private void Start()
+    {
+        _collectibles.Add(this);
+    }
 
     private void ToggleHighlighter(bool visible)
     {
@@ -12,7 +25,36 @@ public class Collectible : MonoBehaviour
 
     private void Collect()
     {
-        Inventory.Instance.AddItem(inventorySprite, false);
-        enabled = false;
+        Inventory.Instance.AddPlatToInventory(false);
+        Destroy(gameObject);
+    }
+
+    public static void MouseClicked()
+    {
+        foreach (var collectible in _collectibles)
+        {
+            collectible.ClickRegistered();
+        }
+    }
+
+    private void ClickRegistered()
+    {
+        if (!_mouseHovering) return;
+        Collect();
+    }
+
+    private void OnMouseOver()
+    {
+        _mouseHovering = true;
+    }
+
+    private void OnMouseExit()
+    {
+        _mouseHovering = false;
+    }
+
+    private void Update()
+    {
+        highlighterSpriteRenderer.enabled = _mouseHovering;
     }
 }
